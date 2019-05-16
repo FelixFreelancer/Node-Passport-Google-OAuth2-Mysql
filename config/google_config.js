802b74185749e93e971b6f6d0ade0fca07801f02
Felix <40544276+FelixFreelancer@users.noreply.github.com>
2019-05-17 01:10:13 +0800

    Update google_config.js

--- config/google_config.js
+++ config/google_config.js
@@ -1,3 +1,3 @@
-exports.CLIENTID = "333941285218-i97vm3r6jcjpf5cobcmmurfvv0mquv0c.apps.googleusercontent.com";
-exports.CLIENTSECRET = "1EGXvv70O8z8DpqjLzqp9o1G";
+exports.CLIENTID = "YOUR CLIENT ID";
+exports.CLIENTSECRET = "YOUR CLIENT KEY";
 exports.CALLBACKURL = "http://localhost:8000/auth/google/callback";
